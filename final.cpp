f4a1d1ab180dfc7158d52a71cf99efa179d0f7d1
Brandon <118803690+McKinley116@users.noreply.github.com>
2024-02-15 23:40:00 -0800

    Initial coding of final project. Im planning to do a C++ learning game...

---
+++ final.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{
+	int userName;
+
+	cout << "Hello, what is your name?" << endl;
+	cin >> userName;
+	cout << "Hello, " << userName << "." << " Welcome to Learning C++!" << endl;	
+
+	return 0;
+}
