432907281aa01c0d64aa97cfe68510d4a89e52b2
Brandon <118803690+McKinley116@users.noreply.github.com>
2024-02-20 22:37:33 -0800

    creation of hwnum2 to rebuild program, inital outline of program..

---
+++ hwnum2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+bool hwDirection(int hwNum) { return hwNum % 2 == 0; }
+
+int main() {
+
+  // user inputs highway number to determine primary, auxiliary, and N/S, E/W
+  // directions.
+  int hwNum;
+  int primaryHw;
+  int auxiliarHw;
+
+  // user inputs highwayNum.
+  std::cout << "Input highway:" << std::endl;
+  std::cin >> hwNum;
+
+  // fuctions determine if highway number is primary, which is numbered from 1
+  // - 99. if auxiliary, numbered from 100 - 999. auxiliary also services
+  // primary highways based on right most two digits. direction of highway is
+  // also determined by odd numbers = N/S. even numbers = E/W.
+
+  return 0;
+}
