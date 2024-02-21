cf91c7a687b8050acc2a9b7a6713c75b65365d95
Brandon <118803690+McKinley116@users.noreply.github.com>
2024-02-20 23:38:25 -0800

    Completed determination of primary.... working of determining auxiliary hw....

--- hwnum2.cpp
+++ hwnum2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ostream>
 #include <string>
 
 // fuctions determine if highway number is primary, which is numbered from 1
@@ -6,14 +7,19 @@
 // primary highways based on right most two digits. direction of highway is
 // also determined by odd numbers = N/S. even numbers = E/W.
 
-bool hwPrimary(int hwNum) { return hwNum % 2 == 0; }
+bool hwPrimary(int hwNum) { return hwNum % 2 == 1; }
+bool hwAuxiliary(int hwNum) { return hwNum % 3 == 0; }
 std::string hwDirection(int hwNum) {
   if (hwNum < 0) {
     return "This is not a highway number.";
   } else if (hwNum == 0) {
     return "This is not a highway number.";
   } else {
-    return (hwNum % 2 == 0) ? "East/West" : "North/South";
+    return (hwNum % 2 == 1) ? "East/West" : "North/South";
+  }
+
+  if (hwNum > 1) {
+    return (hwNum % 3 == 0) ? "East/West" : "North South";
   }
 }
 
@@ -22,13 +28,27 @@ int main() {
   // user inputs highway number to determine primary, auxiliary, and N/S, E/W
   // directions.
 
-  int hwNum;
-  int primaryHw;
-  int auxiliarHw;
-
-  // user inputs highwayNum.
+  int hwNum; // defined highway number
   // std::cout << "Input highway:" << std::endl; --> testing input
-  std::cin >> hwNum;
+  std::cin >> hwNum; // user determines highway number.
+  std::string direction = hwDirection(hwNum);
+  bool primary = hwPrimary(hwNum);
+  bool auxiliary = hwPrimary(hwNum);
 
+  // this determines outputs direction and primary or auxiliary..
+  if (direction != "This is not a highway number.") {
+
+    std::cout << "I-" << hwNum << " is " << (primary ? "Primary" : "Auxiliary")
+              << ", "
+              << "going " << direction << std::endl;
+  } else {
+    std::cout << "Invalid highway number entered." << std::endl;
+  }
+
+  if (direction == hwAuxiliary) {
+    std::cout << "I- " << hwNum << " is "
+              << (auxiliary ? "Primary" : "Auxiliary") << ", "
+              << "it services I-90." << std::endl;
+  }
   return 0;
 }
