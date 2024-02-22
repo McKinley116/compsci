e1d94c0ff282e678e72a0e0612de881503657514
Brandon <118803690+McKinley116@users.noreply.github.com>
2024-02-21 20:41:13 -0800

    saving code..

--- hwnum.cpp
+++ hwnum.cpp
@@ -2,18 +2,45 @@
 #include <ostream>
 #include <string>
 
+bool hwPrimary(int hwNum) { return hwNum >= 1 && hwNum <= 99; }
+bool hwAuxiliary(int hwNum) { return hwNum >= 100 && hwNum <= 999; }
+
+std::string hwDirection(int hwNum) {
+  if (hwNum < 0) {
+    return "This is not a highway number.";
+  } else if (hwNum == 0 || (hwNum % 100 == 0)) {
+    return "This is not a highway number.";
+  } else {
+    return (hwNum % 2 == 0) ? "east/west." : "north/south.";
+  }
+}
+
+std::string getServiceInfo(int hwNum) {
+  if (hwAuxiliary(hwNum)) {
+    int primaryHighway = hwNum % 100;
+    return "serving I-" + std::to_string(primaryHighway) + ", ";
+  }
+  return "";
+}
+
 int main() {
   int hwNum;
-
   std::cin >> hwNum;
 
-  if (hwNum > 1 && hwNum < 99) {
-    std::cout << "I-" << hwNum << " is primary, going east/west." << std::endl;
-  } else if (hwNum > 100 && hwNum < 999) {
-    std::cout << "I-" << hwNum << " is auxiliary, serving " << hwNum << ", "
-              << "going east/west." << std::endl;
+  std::string direction = hwDirection(hwNum);
+  bool primary = hwPrimary(hwNum);
+  bool auxiliary = hwAuxiliary(hwNum);
+
+  if (direction != "This is not a highway number.") {
+    if (primary) {
+      std::cout << "I-" << hwNum << " is primary, ";
+    } else if (auxiliary) {
+      std::cout << "I-" << hwNum << " is auxiliary, serving ";
+      std::cout << "I-" << hwNum % 100 << ", ";
+    }
+    std::cout << "going " << direction << std::endl;
   } else {
-    std::cout << hwNum << " is not a valid highway number." << std::endl;
+    std::cout << "Invalid highway number entered." << std::endl;
   }
 
   return 0;
