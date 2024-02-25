1322be85c7c5fda444ee7baa2f3f836c73445ad7
Brandon <118803690+McKinley116@users.noreply.github.com>
2024-02-24 23:23:21 -0800

    completion of warmup.. need to fix space

--- righttri.cpp
+++ righttri.cpp
@@ -8,11 +8,15 @@ int main() {
   std::cin >> triChar;
   std::cout << "Enter a height: " << std::endl;
   std::cin >> triHeight;
+  std::cout << " " << std::endl;
 
   int height = 0;
-  while (height < triHeight) {
-    std::cout << triChar << std::endl;
-    height++;
+
+  for (int i = 1; i <= triHeight; ++i) { // loops through each row..
+    for (int j = 0; j < i; ++j) {  // loops to print characters in each row..
+      std::cout << triChar << " "; // prints space after characters..
+    }
+    std::cout << std::endl; // moves to next line...
   }
 
   return 0;
