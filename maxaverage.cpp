350d7f9563c2d3f22643b662491287d10a202724
Brandon <118803690+McKinley116@users.noreply.github.com>
2024-02-26 21:41:55 -0800

    initial setup of program.

---
+++ maxaverage.cpp
@@ -0,0 +1,12 @@
+#include <iomanip>
+#include <iostream>
+#include <utility>
+int main() {
+  // define number variables as floating.
+  // user input.
+  // calculate highest number.
+  // calculate average.
+  // end program with negative number, is not included.
+  // output max and average.
+  return 0;
+}
