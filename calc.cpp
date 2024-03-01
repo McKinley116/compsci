87221c787d5aa0d52ae381d6074b47c2cdee9def
Brandon <118803690+McKinley116@users.noreply.github.com>
2024-02-29 18:34:00 -0800

    Layout of calc sections, math code added, sections divided.

--- calc.cpp
+++ calc.cpp
@@ -10,13 +10,13 @@
 #include <iomanip>
 #include <cmath>
 #include <ostream>
+#include <set>
 #include <string>
 
 int main ()
 {
-	std::cout << "-----Panther Express Cashier Program-----" << std::endl;
 	// require user input for three items and their cost and quantity.
-	//	cost per item = unit price * quantity
+	std::cout << "-----Panther Express Cashier Program-----" << std::endl;
 	std::string itmBght1; 
 	double itmPrice1;
 	int itmQnty1;
@@ -44,24 +44,44 @@ int main ()
 	std::cin >> itmPrice3;
 	std::cin >> itmQnty3;
 	std::cout << "Unit name: " << itmBght3 << std::endl;
-	std::cout << " Unit price for " << itmBght3 << " is: " << itmPrice3 << std::endl;
+	std::cout << "Unit price for " << itmBght3 << " is: " << itmPrice3 << std::endl;
 	std::cout << "Unit quantity for " << itmBght3 << " is: " << itmQnty3 << std::endl;
-
+	std::cout << std::endl;
 
 	// require user to input sales tax ( between 0 and 100).
 	int slsTax (slsTax > 0 && slsTax < 100);
 	std::cin >> slsTax;
 	std::cout << "Tax rate (in whole numbers): " << slsTax << std::endl;
+	std::cout << std::endl;
 	// displays break down of items, cost, quantity. cost summary.
+	// cost per item = unit price * quantity
+	int ttlCost1 = itmPrice1 * itmQnty1;
+	int ttlCost2 = itmPrice2 * itmQnty2;
+	int ttlCost3 = itmPrice3 * itmQnty3;
+	std::cout << "-----Cost Breakdown-----" << std::endl;
+	std::cout << "Item" << std::setw(10) << "Price" << std::setw(14) << "Quantity" << std::setw(10) << "Cost" << std::endl;
+	std::cout << itmBght1 << std::setw(10) << itmPrice1 << std::setw(14) << itmQnty1 << std::setw(10) << ttlCost1  << std::endl;
+	std::cout << itmBght2 << std::setw(10) << itmPrice2 << std::setw(14) << itmQnty2 << std::setw(10) << ttlCost2 << std::endl;
+	std::cout << itmBght3 << std::setw(10) << itmPrice3 << std::setw(14) << itmQnty3 << std::setw(10) << ttlCost3 << std::endl;
 	// displays pretax.
-	//	total before tax = sum of all three items.
+	// total before tax = sum of all three items.
+	std::cout << "-----Cost Pretax-----" << std::endl;
+
 	// displays w/ tax.
 	//	tax = total before tax * tax rate
 	//	total after tax = total cost * tax
+	std::cout << "-----Cost w/ Tax-----" << std::endl;
+
 	// askes user price paid.
+	double moneyPd;
+	std::cout << "How much money do you have?" << std::endl;
+	std::cin >> moneyPd;
+	std::cout << moneyPd << std::endl;
+
 	// displays change.
 	//	change = total - cost
 	// displays denominations. ($100, $20, $5, $1, .25, .10, .05, .01)
+	std::cout << "$-----Change-----$" << std::endl;
 
 return 0;
 }
