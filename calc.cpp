df094be3a4b8715603ed27abcfdfb22c4863edc7
Brandon <118803690+McKinley116@users.noreply.github.com>
2024-02-29 18:00:38 -0800

    break up of input items, addition of sales tax input/output.

--- calc.cpp
+++ calc.cpp
@@ -5,9 +5,11 @@
 //Description:  This program will be a cashier calculator for the panther store that will display items, price, quantity, sales tax, etc. 
 //Usage:  This program will ask the user for input, items and cost, amount paid and a table of the information.
 
+#include <cstdio>
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <ostream>
 #include <string>
 
 int main ()
@@ -15,16 +17,41 @@ int main ()
 	std::cout << "-----Panther Express Cashier Program-----" << std::endl;
 	// require user input for three items and their cost and quantity.
 	//	cost per item = unit price * quantity
-	std::string itmBght; 
-	double itmPrice;
-	int itmQnty;
+	std::string itmBght1; 
+	double itmPrice1;
+	int itmQnty1;
+	std::cin >> itmBght1;
+	std::cin >> itmPrice1;
+	std::cin >> itmQnty1;
+	std::cout << "Unit name: " << itmBght1 << std::endl; 
+	std::cout << " Unit price for " << itmBght1 << " is: " << itmPrice1 << std::endl;
+	std::cout << "Unit quantity for " << itmBght1 << " is: " << itmQnty1 << std::endl;
 
-	std::cin >> itmBght;
-	std::cin >> itmPrice;
-	std::cin >> itmQnty;
+	std::string itmBght2;
+	double itmPrice2;
+	int itmQnty2;
+	std::cin >> itmBght2; 
+	std::cin >> itmPrice2;
+	std::cin >> itmQnty2;
+	std::cout << "Unit name: " << itmBght2 << std::endl; 
+	std::cout << " Unit price for " << itmBght2 << " is: " << itmPrice2 << std::endl;
+	std::cout << "Unit quantity for " << itmBght2 << " is: " << itmQnty2 << std::endl;
 
-	std::cout << itmBght << " " << itmPrice << " " << itmQnty << std::endl;
+	std::string itmBght3;
+	double itmPrice3;
+	int itmQnty3;
+	std::cin >> itmBght3;
+	std::cin >> itmPrice3;
+	std::cin >> itmQnty3;
+	std::cout << "Unit name: " << itmBght3 << std::endl;
+	std::cout << " Unit price for " << itmBght3 << " is: " << itmPrice3 << std::endl;
+	std::cout << "Unit quantity for " << itmBght3 << " is: " << itmQnty3 << std::endl;
+
+
 	// require user to input sales tax ( between 0 and 100).
+	int slsTax (slsTax > 0 && slsTax < 100);
+	std::cin >> slsTax;
+	std::cout << "Tax rate (in whole numbers): " << slsTax << std::endl;
 	// displays break down of items, cost, quantity. cost summary.
 	// displays pretax.
 	//	total before tax = sum of all three items.
