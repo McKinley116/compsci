ab86d23ce29b6d2a0be3d405caf26fedda573bea
Brandon <118803690+McKinley116@users.noreply.github.com>
2024-02-29 21:17:41 -0800

    addition of math functions and adjustments of table spacing

--- calc.cpp
+++ calc.cpp
@@ -16,16 +16,17 @@
 int main ()
 {
 	// require user input for three items and their cost and quantity.
-	std::cout << "-----Panther Express Cashier Program-----" << std::endl;
+	std::cout << "$-----Panther Express Cashier Program-----$" << std::endl;
 	std::string itmBght1; 
 	double itmPrice1;
 	int itmQnty1;
 	std::cin >> itmBght1;
-	std::cin >> itmPrice1;
-	std::cin >> itmQnty1;
 	std::cout << "Unit name: " << itmBght1 << std::endl; 
+	std::cin >> itmPrice1;
 	std::cout << " Unit price for " << itmBght1 << " is: " << itmPrice1 << std::endl;
+	std::cin >> itmQnty1;
 	std::cout << "Unit quantity for " << itmBght1 << " is: " << itmQnty1 << std::endl;
+	std::cout << std::endl;
 
 	std::string itmBght2;
 	double itmPrice2;
@@ -36,6 +37,7 @@ int main ()
 	std::cout << "Unit name: " << itmBght2 << std::endl; 
 	std::cout << " Unit price for " << itmBght2 << " is: " << itmPrice2 << std::endl;
 	std::cout << "Unit quantity for " << itmBght2 << " is: " << itmQnty2 << std::endl;
+	std::cout << std::endl;
 
 	std::string itmBght3;
 	double itmPrice3;
@@ -50,38 +52,61 @@ int main ()
 
 	// require user to input sales tax ( between 0 and 100).
 	int slsTax (slsTax > 0 && slsTax < 100);
+
 	std::cin >> slsTax;
 	std::cout << "Tax rate (in whole numbers): " << slsTax << std::endl;
 	std::cout << std::endl;
+
 	// displays break down of items, cost, quantity. cost summary.
 	// cost per item = unit price * quantity
-	int ttlCost1 = itmPrice1 * itmQnty1;
-	int ttlCost2 = itmPrice2 * itmQnty2;
-	int ttlCost3 = itmPrice3 * itmQnty3;
-	std::cout << "-----Cost Breakdown-----" << std::endl;
-	std::cout << "Item" << std::setw(10) << "Price" << std::setw(14) << "Quantity" << std::setw(10) << "Cost" << std::endl;
-	std::cout << itmBght1 << std::setw(10) << itmPrice1 << std::setw(14) << itmQnty1 << std::setw(10) << ttlCost1  << std::endl;
-	std::cout << itmBght2 << std::setw(10) << itmPrice2 << std::setw(14) << itmQnty2 << std::setw(10) << ttlCost2 << std::endl;
-	std::cout << itmBght3 << std::setw(10) << itmPrice3 << std::setw(14) << itmQnty3 << std::setw(10) << ttlCost3 << std::endl;
+	double ttlCost1 = itmPrice1 * itmQnty1;
+	double ttlCost2 = itmPrice2 * itmQnty2;
+	double ttlCost3 = itmPrice3 * itmQnty3;
+	std::cout << "$-----Cost Breakdown-----$" << std::endl;
+	std::cout << "Item" << std::setw(15) << "Price" << std::setw(15) << "Quantity" << std::setw(15) << "Cost" << std::endl;
+	std::cout << itmBght1 << std::setw(15) << itmPrice1 << std::setw(15) << itmQnty1 << std::setw(15) << ttlCost1 << std::endl;
+	std::cout << itmBght2 << std::setw(15) << itmPrice2 << std::setw(15) << itmQnty2 << std::setw(15) << ttlCost2 << std::endl;
+	std::cout << itmBght3 << std::setw(15) << itmPrice3 << std::setw(15) << itmQnty3 << std::setw(15) << ttlCost3 << std::endl;
+	std::cout << std::endl;
+
 	// displays pretax.
 	// total before tax = sum of all three items.
-	std::cout << "-----Cost Pretax-----" << std::endl;
+	double preTotal = ttlCost1 + ttlCost2 + ttlCost3;
+	std::cout << "$-----Cost Pretax-----$" << std::endl;
+	std::cout << preTotal << std::endl;
+	std::cout << std::endl;
 
 	// displays w/ tax.
 	//	tax = total before tax * tax rate
 	//	total after tax = total cost * tax
-	std::cout << "-----Cost w/ Tax-----" << std::endl;
+	std::cout << "$-----Cost w/ Tax-----$" << std::endl;
+	double txRate = slsTax / 2.0;
+	double tax = preTotal * txRate;
+	double total = preTotal * tax;
+	std::cout << "$"<< total << std::endl;
+	std::cout << std::endl;
 
-	// askes user price paid.
+	// asks user price paid.
 	double moneyPd;
-	std::cout << "How much money do you have?" << std::endl;
+	std::cout << "$-----How much money do you have?-----$" << std::endl;
 	std::cin >> moneyPd;
 	std::cout << moneyPd << std::endl;
 
 	// displays change.
 	//	change = total - cost
-	// displays denominations. ($100, $20, $5, $1, .25, .10, .05, .01)
+	double change = moneyPd - total;
 	std::cout << "$-----Change-----$" << std::endl;
+	std::cout << change << std::endl;
+	std::cout << "--------------------";
+	std::cout << std::endl;
+
+	// displays denominations of %100, $20, $5, $1, .25, .10, .05, .01).
+	const int DOLLAR = 100;
+	const int QUARTER = 25;
+	const int DIME = 10;
+	const int NICKEL = 5;
+	const int PENNY = 1;
+	std::cout << "Denominations" << std::endl;
 
 return 0;
 }
